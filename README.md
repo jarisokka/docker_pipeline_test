f6b679883788691d0a1b865ff947f7f9be378857
Jari Sokka <sokkajar@gmail.com>
2021-09-13 13:06:36 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DevOps with Docker
+
+This material is for practice use to test deployment pipeline to heroku with docker.
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const App = () => (
+  <div>
+    <h1>Deployment pipeline to heroku</h1>
+  </div>
+)
+
+export default App
